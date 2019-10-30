59003542a67370a02b1c73dbd312dac6a00ad5d8
VIK2395 <50545334+VIK2395@users.noreply.github.com>
2019-10-30 21:23:50 +0200

    toCheck

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+let	counters = [
+		{ id: 1, value: 4 },
+		{ id: 2, value: 0 },
+		{ id: 3, value: 0 },
+		{ id: 4, value: 0 },
+	];
+
+let stringJson = JSON.stringify(counters);
+
+let savebtn = document.getElementById("save");
+
+savebtn.onclick = function() {downloadValue(stringJson)}
+
+function downloadValue(textToSave) {
+	let hiddenLinkElement = document.createElement('a');
+		hiddenLinkElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+		// hiddenLinkElement.target = '_blank';
+		hiddenLinkElement.download = 'saved.txt';//json
+		hiddenLinkElement.click();
+}
