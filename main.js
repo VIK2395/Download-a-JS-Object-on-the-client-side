b976ca381b137fcbb5e22b6d489b442ca75e5fe5
VIK2395 <50545334+VIK2395@users.noreply.github.com>
2019-11-03 17:13:06 +0200

    code formatting done

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-﻿'use strict';
+'use strict';
 
-let	counters = [
-		{ id: 1, value: 4 },
-		{ id: 2, value: 0 },
-		{ id: 3, value: 0 },
-		{ id: 4, value: 0 },
-	];
+let counters = [
+    { id: 1, value: 4 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+];
 
 let stringJson = JSON.stringify(counters);
 
 let savebtn = document.getElementById("save");
 
-savebtn.onclick = function() {downloadValue(stringJson)}
+savebtn.onclick = function () { downloadFile(stringJson) }
 
-function downloadValue(textToSave) {
-	let hiddenLinkElement = document.createElement('a');
-		hiddenLinkElement.href = 'data:attachment/text,' + encodeURI(textToSave);
-		// hiddenLinkElement.target = '_blank';
-		hiddenLinkElement.download = 'saved.txt';//json
-		hiddenLinkElement.click();
+function downloadFile(textToSave) {
+    let hiddenLinkElement = document.createElement('a');
+    hiddenLinkElement.href = 'data:text/plain,' + textToSave;
+    hiddenLinkElement.download = 'savedObj.txt';
+    hiddenLinkElement.click();
 }
